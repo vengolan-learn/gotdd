36cebf3646718ede5f0cd2e5fe0cca5491966873
vengolan <vengolan@gmail.com>
2021-06-28 13:46:09 +0530

    Added basic implementation of witdhraw function. And, refactored test

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -7,14 +7,27 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
-	(wallet).Deposit(Bitcoin(10))
-	got := (wallet).Balance()
-	want := Bitcoin(11)
-
-	fmt.Printf("address of balancein test is %v \n", &wallet.balance)
-	if got != want {
-		t.Errorf("want %s, got %s", want, got)
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("want %s, got %s", want, got)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		(wallet).Deposit(Bitcoin(10))
+		want := Bitcoin(10)
+
+		fmt.Printf("address of balancein test is %v \n", &wallet.balance)
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraow", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(8))
+		want := Bitcoin(12)
+		assertBalance(t, wallet, want)
+	})
 
 }
