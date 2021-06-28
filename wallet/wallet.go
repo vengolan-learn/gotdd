e7a3b6b8a37b1a52639bf90f7015ea0365f55c2a
vengolan <vengolan@gmail.com>
2021-06-28 12:55:03 +0530

    Initial Setup

---
+++ wallet/wallet.go
@@ -0,0 +1,16 @@
+package main
+
+type Wallet struct{}
+
+func (w Wallet) Deposit(amount int) {
+
+}
+
+func (w Wallet) Balance() int {
+	return 0
+
+}
+
+func main() {
+
+}
---
+++ wallet/wallet_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+
+	wallet := Wallet{}
+	wallet.Deposit(10)
+	got := wallet.Balance()
+	want := 10
+
+	if got != want {
+		t.Errorf("want %d, got %d", want, got)
+	}
+
+}
