7897f6f2b02faef68590548e906061191d485afd
vengolan <vengolan@gmail.com>
2021-07-02 15:38:55 +0530

    used httptest.newserver instead of external urls for testing

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,15 +1,33 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://facebook.com"
-	fastURL := "htp://www.quii.co.uk"
+
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
 	if got != want {
 		t.Errorf("want %q, but got %q", want, got)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 
 }
