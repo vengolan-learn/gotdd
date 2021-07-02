de1e058f0664edc3f78d7bdae82bb7d240a7a73c
vengolan <vengolan@gmail.com>
2021-07-02 15:12:00 +0530

    used channels to pass and receive results

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -2,12 +2,25 @@ package concurrency
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultchan := make(chan result)
+
 	for _, url := range urls {
-		go func() {
-			results[url] = wc(url)
-		}()
+		go func(u string) {
+			resultchan <- result{u, wc(u)}
+		}(url)
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultchan
+		results[r.string] = r.bool
+
 	}
+
 	return results
 }
