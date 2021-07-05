8212470d3b5f942293df6ef5f83211acb9e25ea9
vengolan <vengolan@gmail.com>
2021-07-05 15:32:50 +0530

    sync - Initial commit

---
+++ sync/sync_test.go
@@ -0,0 +1,43 @@
+package synctdd
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+		assertCounter(t, counter, wantedCount)
+
+	})
+
+	t.Run("incrementing th ecounter 3 times eaves it at 3", func(t *testing.T) {
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		assertCounter(t, counter, 3)
+	})
+
+}
+
+func assertCounter(t testing.TB, got Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("want %d, but got %d", want, got.Value())
+	}
+}
