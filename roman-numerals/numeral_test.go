5548d7c39c1dadb052bbafeec81afe124449242f
vengolan <vengolan@gmail.com>
2021-07-07 21:53:30 +0530

    roman - refactor - build till 100

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -14,6 +14,15 @@ func TestRomanNumeral(t *testing.T) {
 		{"3 gets converted to II", 3, "III"},
 		{"4 gets converted to II", 4, "IV"},
 		{"5 gets converted to II", 5, "V"},
+		{"6 gets converted to II", 6, "VI"},
+		{"7 gets converted to II", 7, "VII"},
+		{"8 gets converted to II", 8, "VIII"},
+		{"9 gets converted to II", 9, "IX"},
+		{"14 gets converted to II", 14, "XIV"},
+		{"18 gets converted to II", 18, "XVIII"},
+		{"19 gets converted to II", 19, "XIX"},
+		{"20 gets converted to II", 20, "XX"},
+		{"39 gets converted to II", 39, "XXXIX"},
 	}
 
 	for _, test := range cases {
