1df759b379fa1c4e70751f2ede33db478a801cca
vengolan <vengolan@gmail.com>
2021-07-07 21:41:29 +0530

    roman - fix tests till 5

--- roman-numerals/roman_numeral.go
+++ roman-numerals/roman_numeral.go
@@ -1,6 +1,26 @@
 package numeral
 
-func ConvertToRoman(arabic int) string {
-	return "I"
+import "strings"
 
+func ConvertToRoman(arabic int) string {
+
+	var result strings.Builder
+
+	for i := arabic; i > 0; i-- {
+
+		if i == 5 {
+			result.WriteString("V")
+			break
+
+		}
+
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
+
+		result.WriteString("I")
+
+	}
+	return result.String()
 }
